7c47e1858f07f341df0357f9656b8f60810940b1
mohamed othman <mohamed.othman@paysky.io>
2023-08-19 18:36:49 +0300

    update swagger docs

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Mini-project “AIR QUALITY”')
+    .setDescription('Mini-project “AIR QUALITY” API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('AIR QUALITY')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
